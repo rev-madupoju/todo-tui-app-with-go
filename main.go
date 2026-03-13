56b0c2224b498152efc4c58a1d6a1f842df96836
Revanth Madupoju <revanthmadupoju@gmail.com>
2026-03-13 17:05:07 +0500

    completed build

--- main.go
+++ main.go
@@ -2,18 +2,9 @@ package main
 
 func main() {
 	todos := Todos{}
-	
-	todos.add("Clean my desk")
-	todos.add("Write a blog")
-	todos.add("Read the Go docs")
-	todos.add("Check out awesome-rust projs on gh")
-	todos.add("Go to gym at 5PM")
-
-	todos.toggle(0) // mark "Clean my desk as completed"
-
-	todos.delete(2) // delete "Read the Go docs"
-
-	todos.edit(2, "Check out awesome-rust github projects ASAP")
-
-	todos.print()
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
+	cmdFlags := NewCmdFlags()
+	cmdFlags.Execute(&todos)
+	storage.Save(todos)
 }
